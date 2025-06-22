8250aa5ec2ef93d5b04e47f82e2a4c2749b7984a
Kaitachi92 <kaitachi92@email.com>
2025-06-22 20:29:38 -0300

    Limpeza total do repositório

--- node-pg-migrate.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  databaseUrl: 'postgres://admin:admin@localhost:5432/escola',
-  migrationsTable: 'pgmigrations',
-  dir: 'migrations',
-  direction: 'up',
-  count: Infinity,
-};
